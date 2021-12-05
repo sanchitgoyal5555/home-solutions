151bee9bffa8f7522f10709cd1ca0744d090d1db
Naman Jain <56696427+namanjain96690@users.noreply.github.com>
2021-12-05 22:44:04 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-let btn= document.querySelector(".signupbtn");
-btn.addEventListener("click",fun);
-function fun(){
-    this.textContent='You are Registered';
-    document.getElementById("name").value=" ";
-    document.getElementById("email").value=" ";
-}
-
